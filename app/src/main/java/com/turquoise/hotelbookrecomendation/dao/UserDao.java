2bf510edc99b6ea0f26d143de518acfb56ae458e
Georgi <georgi_01_kovachev@abv.bg>
2023-04-30 11:39:04 +0300

    Add exit confirmation dialog

--- app/src/main/java/com/turquoise/hotelbookrecomendation/dao/UserDao.java
+++ app/src/main/java/com/turquoise/hotelbookrecomendation/dao/UserDao.java
@@ -1,26 +1,26 @@
-package com.turquoise.hotelbookrecomendation.dao;
-
-import androidx.room.Dao;
-import androidx.room.Delete;
-import androidx.room.Insert;
-import androidx.room.Query;
-
-import com.turquoise.hotelbookrecomendation.model.User;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.List;
-
-@Dao
-public interface UserDao {
-    @Query("SELECT * FROM users")
-    List<User> getAll();
-
-    @Insert
-    void insertUser(User user);
-
-
-    @Delete
-    void delete(User user);
-}
+//package com.turquoise.hotelbookrecomendation.dao;
+//
+//import androidx.room.Dao;
+//import androidx.room.Delete;
+//import androidx.room.Insert;
+//import androidx.room.Query;
+//
+//import com.turquoise.hotelbookrecomendation.model.User;
+//
+//import java.sql.Connection;
+//import java.sql.PreparedStatement;
+//import java.sql.SQLException;
+//import java.util.List;
+//
+//@Dao
+//public interface UserDao {
+//    @Query("SELECT * FROM users")
+//    List<User> getAll();
+//
+//    @Insert
+//    void insertUser(User user);
+//
+//
+//    @Delete
+//    void delete(User user);
+//}
